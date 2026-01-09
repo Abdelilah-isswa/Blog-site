edaf6963721f0b0cfe2a9ba1d877279ede646130
Abdelilah Isswa <tomhary66@gmail.com>
2026-01-09 19:34:14 +0100

    add the bd connection to user class

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,15 @@ use App\Config\Database;
 class User
 {
 
-
+    private static $conn = null;
     
+    private static function getConnection()
+    {
+        if (self::$conn === null) {
+            $database = new Database();
+            self::$conn = $database->connect();
+        }
+        return self::$conn;
+    }
+
 }
