8929f7dc401b742ff795ec1b570a145a235886d7
Abdelilah Isswa <tomhary66@gmail.com>
2026-01-09 19:40:43 +0100

    add the add and remouve like function

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -5,8 +5,35 @@ use App\Config\Database;
 
 class Like
 {
-
-
-
+private static $conn = null;
+public static function add($articleId, $userId)
+{
+    $conn = self::getConnection();
     
+     
+    if (self::isLiked($userId, $articleId)) {
+        return false; 
+    }
+    
+    $sql = "INSERT INTO Likes (article_id, user_id) VALUES (:article_id, :user_id)";
+    $stmt = $conn->prepare($sql);
+    return $stmt->execute([
+        'article_id' => $articleId,
+        'user_id' => $userId
+    ]);
+}
+
+ 
+public static function remove($articleId, $userId)
+{
+    $conn = self::getConnection();
+    $sql = "DELETE FROM Likes WHERE article_id = :article_id AND user_id = :user_id";
+    $stmt = $conn->prepare($sql);
+    return $stmt->execute([
+        'article_id' => $articleId,
+        'user_id' => $userId
+    ]);
+}
+
+
 }
