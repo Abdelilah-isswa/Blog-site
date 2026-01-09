06bb7436799a19df9ce0390a9ec7cc28cf65a4ff
Abdelilah Isswa <tomhary66@gmail.com>
2026-01-09 19:35:09 +0100

    add the find functiones in user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,4 +18,34 @@ class User
         return self::$conn;
     }
 
+    public static function find($id)
+    {
+        $conn = self::getConnection();
+        $sql = "SELECT * FROM User WHERE user_id = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch();
+    }
+    
+    public static function findByEmail($email)
+    {
+        $conn = self::getConnection();
+        $sql = "SELECT * FROM User WHERE email = :email LIMIT 1";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['email' => $email]);
+        return $stmt->fetch();
+    }
+    
+    public static function findByUsername($username)
+    {
+        $conn = self::getConnection();
+        $sql = "SELECT * FROM User WHERE username = :username LIMIT 1";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['username' => $username]);
+        return $stmt->fetch();
+    }
+    
+
+
+
 }
