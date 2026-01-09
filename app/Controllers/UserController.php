9cbc34600545e0555e2bfc012b175df86d53e30c
Abdelilah Isswa <tomhary66@gmail.com>
2026-01-09 19:29:55 +0100

    add login function

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -8,6 +8,21 @@ use App\Models\User;
 class UserController extends Controller
 {
 
+    public function login()
+    {
+        if (isset($_SESSION['user_id'])) {
+            header('Location: /blog2/public/');
+            exit;
+        }
+        
+        $this->view('login', [
+            'title' => 'Login',
+            'user' => null,
+            'error' => $_SESSION['error'] ?? null
+        ]);
+        
+        unset($_SESSION['error']);
+    }
 
 
 
@@ -15,5 +30,4 @@ class UserController extends Controller
 
 
 
-    
 }
