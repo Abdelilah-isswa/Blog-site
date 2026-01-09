13b83fd7ce721d3295ac77acd805d0ff209c86ce
Abdelilah Isswa <tomhary66@gmail.com>
2026-01-09 19:53:39 +0100

    add the function create

--- app/Controllers/CategoryController.php
+++ app/Controllers/CategoryController.php
@@ -29,4 +29,25 @@ class CategoryController extends Controller
             ]
         ]);
     }
+
+    public function create()
+    {
+        
+        if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'Admin') {
+            $_SESSION['error'] = 'Only admins can create categories';
+            header('Location: /blog2/public/');
+            exit;
+        }
+        
+        $this->view('creatcategories', [
+            'title' => 'Create New Category',
+            'user' => [
+                'id' => $_SESSION['user_id'],
+                'name' => $_SESSION['user_name'] ?? 'Admin',
+                'role' => $_SESSION['user_role'] ?? 'Reader'
+            ]
+        ]);
+    }
+
+
 }
